fc1c74bce09298cbaca8e3a865ab45e81b043801
Elise Bonner <elise0209@mac.com>
2018-09-25 16:13:09 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWxpc2UwMjA5IiwiYSI6ImNqbWk1M24wMTAxY3ozcW9lZ21wYWllNnoifQ.irRFHwg8dbJd53JS2DxCRQ';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
